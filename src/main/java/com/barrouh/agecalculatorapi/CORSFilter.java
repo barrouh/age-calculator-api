6cc4e177b7e568f947e69f9c6a2cdb9fc4a49ac6
Mohamed Barrouh <mohamed.barrouh@gmail.com>
2018-11-06 17:05:29 +0000

    add CORSFilter to allow CORS access control allow origin from all domains

---
+++ src/main/java/com/barrouh/agecalculatorapi/CORSFilter.java
@@ -0,0 +1,33 @@
+package com.barrouh.agecalculatorapi;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.web.filter.GenericFilterBean;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class CORSFilter extends GenericFilterBean implements Filter {
+
+	private static final Logger LOGGER = LogManager.getLogger(CORSFilter.class);
+
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpResponse.setHeader("Access-Control-Allow-Methods", "POST, GET");
+        httpResponse.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content-Type, Accept, X-Auth-Token, X-Csrf-Token, WWW-Authenticate, Authorization");
+        httpResponse.setHeader("Access-Control-Expose-Headers", "custom-token1, custom-token2");
+        httpResponse.setHeader("Access-Control-Allow-Credentials", "false");
+        httpResponse.setHeader("Content-Type", "application/json");
+        httpResponse.setHeader("Access-Control-Max-Age", "3600");
+
+        LOGGER.debug("********** CORS Configuration Completed **********");
+        chain.doFilter(request, response);
+    }
+
+
+}
